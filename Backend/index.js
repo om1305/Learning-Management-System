577d44fc3086d06d2e28625d9cb1173bc8d9d005
om1305 <om.agarwal.hero@gmail.com>
2026-06-30 23:17:56 +0530

    db connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import DBconnect from "./src/Config/DB.js";
 const app = express();
+dotenv.config();
+
 app.listen(5000,()=>{
+    DBconnect();
     console.log("server started");
 })
---
+++ Backend/src/Config/DB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const DBconnect = async() =>{
+    try { 
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("DB connected succesfully");
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+export default DBconnect;
